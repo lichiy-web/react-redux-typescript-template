1e3f583b3c146d6956a478a954f14b4b7025332c
Yevhen Lykhosherstov <lichiy.web.dvlp@gmail.com>
2025-12-26 00:32:38 +0200

    feature/add-locale-selector-to-AppLanguageSync

--- src/components/App/AppLanguageSync.tsx
+++ src/components/App/AppLanguageSync.tsx
@@ -2,10 +2,12 @@ import { useEffect } from 'react';
 // import { useSelector } from 'react-redux';
 import i18n from '@/i18n/i18n';
 // import { selectLocale } from '@/store/selectors';
+import stateApi from '@/store';
+import { useSelector } from 'react-redux';
 
 export function AppLanguageSync() {
   //   const locale = useSelector(selectLocale);
-  const locale = null;
+  const locale = useSelector(stateApi.app.selectors.selectLocale);
   //   console.log({ curLang: i18n.language });
   useEffect(() => {
     if (locale && i18n.language !== locale) {
--- src/store/app/selectors.ts
+++ src/store/app/selectors.ts
@@ -3,3 +3,4 @@ import type { RootState } from '../store';
 export const selectIsLoading = (state: RootState) => state.app.isLoading;
 export const selectError = (state: RootState) => state.app.error;
 export const selectUsersSearchQuery = (state: RootState) => state.app.usersSearchQuery;
+export const selectLocale = (state: RootState) => state.app.locale;
